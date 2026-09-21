2f243a58f5e2fe8aba25667d964faef36606597a
ompa <nmaladina@hotmail.com>
2026-09-21 15:39:16 +1200

    Configure Github Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  basePath: "/kirap-pairap",
+  assetPrefix: "/kirap-pairap/",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
